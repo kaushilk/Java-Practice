88b1752811c4ef58775988352a46c7952b3d8916
kaushilk <kkash321@gmail.com>
2019-04-24 14:11:58 -0400

    master

---
+++ hello/src/hello/application.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		int myNumber;
+		myNumber = 88;
+				
+		System.out.println(myNumber);
+
+	}
+
+}
